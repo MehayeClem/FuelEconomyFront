60cfafebed9ec9ad2d210158b5fcdbb540ad2809
Skynix <romain.ludovic.day@gmail.com>
2023-09-03 20:24:54 +0200

    feat(theme): theme handling in css, scss compile in next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
 import { ThemeProvider } from 'next-themes';
-import '../sass/main.scss';
+import '../styles/main.scss';
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,20 @@
 export default function Home() {
-	return <h1>Home Page</h1>;
+	return (
+		<>
+			<h1>Exemple de Thème Sombre</h1>
+
+			<div className="card">
+				<h1 className="headline">Headline</h1>
+				<span className="subheadline">Subheadline</span>
+				<p className="text">Text</p>
+			</div>
+
+			<button className="button">Button</button>
+			<div>
+				<span className="error">Error</span>
+				<span className="warning">Warning</span>
+				<span className="success">Success</span>
+			</div>
+		</>
+	);
 }
