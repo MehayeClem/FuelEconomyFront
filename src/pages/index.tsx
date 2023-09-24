dba3d0c5eed5a8151a9d4a08780d0022e0541be9
Skynix <romain.ludovic.day@gmail.com>
2023-09-24 10:41:07 +0200

    feat(home): merge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,142 +7,135 @@ import { FaMapLocationDot } from 'react-icons/fa6';
 import { LatLngExpression } from 'leaflet';
 import dynamic from 'next/dynamic';
 
-interface Fuel {
-  name: string;
-  Price: {
-    text: string;
-    currency: string;
-  };
-}
+// interface Fuel {
+//   name: string;
+//   Price: {
+//     text: string;
+//     currency: string;
+//   };
+// }
 
-interface GasStation {
-  id: string;
-  name: string;
-  Fuels: Fuel[];
-  Coordinates: {
-    latitude: number;
-    longitude: number;
-  };
-}
+// interface GasStation {
+//   id: string;
+//   name: string;
+//   Fuels: Fuel[];
+//   Coordinates: {
+//     latitude: number;
+//     longitude: number;
+//   };
+// }
+// var userPos : GeolocationPosition;
+// let userStation = [];
+// let gasStation = [];
+// var map;
 
+// useEffect(() => {
+// 	if (Cookies.get('refresh-token')) {
+// 		toast.success('Bienvenu !');
+// 	}
+// 	console.log("coucou")
+// 	getpos();
+// }, []);
 
-export default function Home() {
-	var userPos : GeolocationPosition;
-	let userStation = [];
-	let gasStation = [];
-	var map;
+// function getpos()
+// {
+// 	navigator.geolocation.getCurrentPosition(savePos, () => {console.log('Erreur')}, {maximumAge : 1000});
+// }
 
-	useEffect(() => {
-		if (Cookies.get('refresh-token')) {
-			toast.success('Bienvenu !');
-		}
-		console.log("coucou")
-		getpos();
-	}, []);
+// function savePos(position)
+// {
+// 	userPos = position;
+// }
 
-	function getpos()
-	{
-		navigator.geolocation.getCurrentPosition(savePos, () => {console.log('Erreur')}, {maximumAge : 1000});
-	}
+// function initMap()
+// {
+// 	// L.mapquest.key = 'SP1kMeiC5ipl9Kqd3BvzCnlJSuccOWJo';
+// 	L.mapquest.key = 'ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7';
 
-	function savePos(position)
-	{
-		userPos = position;
-	}
-	
-	function initMap()
-	{
-		// L.mapquest.key = 'SP1kMeiC5ipl9Kqd3BvzCnlJSuccOWJo';
-		L.mapquest.key = 'ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7';
-		
-		// 'map' refers to a <div> element with the ID map
-		map = L.mapquest.map('map', {
-			center: [userPos.coords.latitude, userPos.coords.longitude],
-			layers: L.mapquest.tileLayer('map'),
-			zoom: 12
-			});
-		
-		L.mapquest.textMarker([userPos.coords.latitude, userPos.coords.longitude], {
-			text: "Moi",
-			subtext: 'Ma position',
-			position: 'right',
-			type: 'marker',
-			icon: {
-			primaryColor: '#F63838',
-			secondaryColor: '#949090',
-			size: 'sm'
-			}}).addTo(map);
-	}
+// 	// 'map' refers to a <div> element with the ID map
+// 	map = L.mapquest.map('map', {
+// 		center: [userPos.coords.latitude, userPos.coords.longitude],
+// 		layers: L.mapquest.tileLayer('map'),
+// 		zoom: 12
+// 		});
 
-	async function getStation()
-	{
-		const response = await axios.get(
-			`https://api.prix-carburants.2aaz.fr/stations/around/${userPos.coords.latitude},${userPos.coords.longitude}`
-		);
-		console.log(response);
-		userStation = response.data;
-	}
-	
-	async function getGasInfo()
-	{
-		for(let i = 0; i < userStation.length; i++)
-		{
-			try 
-			{
-			const response = await axios.get(
-				`https://api.prix-carburants.2aaz.fr/station/${userStation[i].id}`
-			);
-			gasStation[i] = response.data;
-		}catch{
-				console.log('Erreur de demande d\'acces')
-				return;
-			}
-		}
-	}
+// 	L.mapquest.textMarker([userPos.coords.latitude, userPos.coords.longitude], {
+// 		text: "Moi",
+// 		subtext: 'Ma position',
+// 		position: 'right',
+// 		type: 'marker',
+// 		icon: {
+// 		primaryColor: '#F63838',
+// 		secondaryColor: '#949090',
+// 		size: 'sm'
+// 		}}).addTo(map);
+// }
 
-	
-	function addStationMap()
-	{
-		var pop = [];
-		for(let i = 0; i < gasStation.length; i++)
-		{
-			pop[i] = L.popup('popupPane');
-			let str = `<h2>${gasStation[i].name}</h2>\n`;
-			for(let j = 0; j < gasStation[i].Fuels.length; j++)
-			{
-				str +=`<h3>${gasStation[i].Fuels[j].name}</h3>\n`;
-				// console.log(str);
-				str += `<p>Prix : ${gasStation[i].Fuels[j].Price.text} ${gasStation[i].Fuels[j].Price.currency}</p>\n`;
-				// console.log(str);
-			}
-			pop[i].setContent(str);
+// async function getStation()
+// {
+// 	const response = await axios.get(
+// 		`https://api.prix-carburants.2aaz.fr/stations/around/${userPos.coords.latitude},${userPos.coords.longitude}`
+// 	);
+// 	console.log(response);
+// 	userStation = response.data;
+// }
 
-			L.mapquest.textMarker([gasStation[i].Coordinates.latitude, gasStation[i].Coordinates.longitude], {
-				text: gasStation[i].name,
-				subtext: 'Station Essence',
-				position: 'right',
-				type: 'marker',
-				icon: {
-				primaryColor: '#333333',
-				secondaryColor: '#333333',
-				size: 'sm'
-				}
-			}).bindPopup(pop[i]).addTo(map);
-		}
-	}
+// async function getGasInfo()
+// {
+// 	for(let i = 0; i < userStation.length; i++)
+// 	{
+// 		try
+// 		{
+// 		const response = await axios.get(
+// 			`https://api.prix-carburants.2aaz.fr/station/${userStation[i].id}`
+// 		);
+// 		gasStation[i] = response.data;
+// 	}catch{
+// 			console.log('Erreur de demande d\'acces')
+// 			return;
+// 		}
+// 	}
+// }
 
-	return (
-		<>
+// function addStationMap()
+// {
+// 	var pop = [];
+// 	for(let i = 0; i < gasStation.length; i++)
+// 	{
+// 		pop[i] = L.popup('popupPane');
+// 		let str = `<h2>${gasStation[i].name}</h2>\n`;
+// 		for(let j = 0; j < gasStation[i].Fuels.length; j++)
+// 		{
+// 			str +=`<h3>${gasStation[i].Fuels[j].name}</h3>\n`;
+// 			// console.log(str);
+// 			str += `<p>Prix : ${gasStation[i].Fuels[j].Price.text} ${gasStation[i].Fuels[j].Price.currency}</p>\n`;
+// 			// console.log(str);
+// 		}
+// 		pop[i].setContent(str);
 
-		<div>
-			<button className='button' onClick={()=> fetchUserPosition()}>userPosition</button>
-		</div>
-		<div>
-			<button className='button' onClick={()=> initializeMap()}>initMap</button>
-		</div>
+// 		L.mapquest.textMarker([gasStation[i].Coordinates.latitude, gasStation[i].Coordinates.longitude], {
+// 			text: gasStation[i].name,
+// 			subtext: 'Station Essence',
+// 			position: 'right',
+// 			type: 'marker',
+// 			icon: {
+// 			primaryColor: '#333333',
+// 			secondaryColor: '#333333',
+// 			size: 'sm'
+// 			}
+// 		}).bindPopup(pop[i]).addTo(map);
+// 	}
+// }
+{
+	/* // {
+			// <>
 
-			{
-			
+		// <div>
+		// 	<button className='button' onClick={()=> fetchUserPosition()}>userPosition</button>
+		// </div>
+		// <div>
+		// 	<button className='button' onClick={()=> initializeMap()}>initMap</button>
+		// </div>
 			/* <div>
 				<button className="button" onClick={() => getStation()}> Get station aroud me</button>
 			</div>
@@ -153,6 +146,33 @@ export default function Home() {
 			
 			<div>
 				<button className="button" onClick={() => console.log(userStation)}> Print station</button>
+		
+			<div>
+				<button className="button" onClick={() => initMap()}> Affiche Map</button>
+			</div>
+
+			<div>
+				<button className="button" onClick={() => addStationMap()}> Affiche station sur MAP</button>
+			</div>
+
+			<div>
+				<button className="button" onClick={() => getGasInfo()}> Bouton test</button>
+			</div>
+
+			<div id="map">
+			</div>
+		</>
+	);
+	} */
+}
+export default function Home() {
+	const [mapCenter, setMapCenter] = useState<LatLngExpression>([
+		46.603354, 1.888334
+	]);
+	const [zoom, setZoom] = useState<number>(6);
+
+	const Map = dynamic(() => import('../components/map'), { ssr: false });
+	return (
 		<section className="home__container">
 			<div className="search__container">
 				<div className="search__controller">
@@ -176,21 +196,7 @@ export default function Home() {
 				<Map center={mapCenter} zoom={zoom}></Map>
 				<div className="map__infos"></div>
 			</div>
-
-			<div>
-				<button className="button" onClick={() => initMap()}> Affiche Map</button>
-			</div>
-
-			<div>
-				<button className="button" onClick={() => addStationMap()}> Affiche station sur MAP</button>
-			</div>
-
-			<div>
-				<button className="button" onClick={() => getGasInfo()}> Bouton test</button>
-			</div>
-
-			<div id="map">
-			</div>
-		</>
+			)
+		</section>
 	);
-	}
+}
