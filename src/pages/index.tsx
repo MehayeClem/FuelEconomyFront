0cb01b407e0fb447979c968b76285d3cbd480a90
Clément MEHAYE <34130023+MehayeClem@users.noreply.github.com>
2023-09-24 10:14:04 +0200

    Refacto Code quentin + debut map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,132 +1,136 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import { toast } from 'react-toastify';
-import axios, { AxiosInstance } from 'axios';
+import axios from 'axios';
+
+interface Fuel {
+  name: string;
+  Price: {
+    text: string;
+    currency: string;
+  };
+}
+
+interface GasStation {
+  id: string;
+  name: string;
+  Fuels: Fuel[];
+  Coordinates: {
+    latitude: number;
+    longitude: number;
+  };
+}
 
 export default function Home() {
-	var userPos : GeolocationPosition;
-	let userStation = [];
-	let gasStation = [];
-	var map;
+  const [userPosition, setUserPosition] = useState<GeolocationPosition | null>(null);
+  const [nearbyStations, setNearbyStations] = useState<GasStation[]>([]);
+  const [stationDetails, setStationDetails] = useState<GasStation[]>([]);
+  const [mapInstance, setMapInstance] = useState<any | null>(null);
 
-	useEffect(() => {
-		if (Cookies.get('refresh-token')) {
-			toast.success('Bienvenu !');
-		}
-		console.log("coucou")
-		getpos();
-	}, []);
+  useEffect(() => {
+    if (Cookies.get('refresh-token')) {
+      toast.success('Bienvenue !');
+    }
+  }, []);
 
-	function getpos()
-	{
-		navigator.geolocation.getCurrentPosition(savePos, () => {console.log('Erreur')}, {maximumAge : 1000});
-	}
+  function fetchUserPosition() {
+    navigator.geolocation.getCurrentPosition(setUserPosition, () => console.log('Erreur lors de la récupération de la position'), { maximumAge: 1000 });
+  }
 
-	function savePos(position)
-	{
-		userPos = position;
+  function initializeMap() {
+    if (!userPosition) return;
+    
+    L.mapquest.key = 'ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7';
+    
+	if (map){
+		map.remove();
 	}
-	
-	function initMap()
-	{
-		// L.mapquest.key = 'SP1kMeiC5ipl9Kqd3BvzCnlJSuccOWJo';
-		L.mapquest.key = 'ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7';
-		
-		// 'map' refers to a <div> element with the ID map
-		map = L.mapquest.map('map', {
-			center: [userPos.coords.latitude, userPos.coords.longitude],
-			layers: L.mapquest.tileLayer('map'),
-			zoom: 12
-			});
-		
-		L.mapquest.textMarker([userPos.coords.latitude, userPos.coords.longitude], {
-			text: "Moi",
-			subtext: 'Ma position',
-			position: 'right',
-			type: 'marker',
-			icon: {
-			primaryColor: '#F63838',
-			secondaryColor: '#949090',
-			size: 'sm'
-			}}).addTo(map);
-	}
+    const map = L.mapquest.map('map', {
+      center: [userPosition.coords.latitude, userPosition.coords.longitude],
+      layers: L.mapquest.tileLayer('map'),
+      zoom: 12
+    });
 
-	async function getStation()
-	{
-		const response = await axios.get(
-			`https://api.prix-carburants.2aaz.fr/stations/around/${userPos.coords.latitude},${userPos.coords.longitude}`
-		);
-		console.log(response);
-		userStation = response.data;
-	}
-	
-	async function getGasInfo()
-	{
-		for(let i = 0; i < userStation.length; i++)
-		{
-			try 
-			{
-			const response = await axios.get(
-				`https://api.prix-carburants.2aaz.fr/station/${userStation[i].id}`
-			);
-			gasStation[i] = response.data;
-		}catch{
-				console.log('Erreur de demande d\'acces')
-				return;
-			}
-		}
-	}
+    L.mapquest.textMarker([userPosition.coords.latitude, userPosition.coords.longitude], {
+      text: "Moi",
+      subtext: 'Ma position',
+      position: 'right',
+      type: 'marker',
+      icon: {
+        primaryColor: '#F63838',
+        secondaryColor: '#949090',
+        size: 'sm'
+      }
+    }).addTo(map);
 
-	
-	function addStationMap()
-	{
-		var pop = [];
-		for(let i = 0; i < gasStation.length; i++)
-		{
-			pop[i] = L.popup('popupPane');
-			let str = `<h2>${gasStation[i].name}</h2>\n`;
-			for(let j = 0; j < gasStation[i].Fuels.length; j++)
-			{
-				str +=`<h3>${gasStation[i].Fuels[j].name}</h3>\n`;
-				// console.log(str);
-				str += `<p>Prix : ${gasStation[i].Fuels[j].Price.text} ${gasStation[i].Fuels[j].Price.currency}</p>\n`;
-				// console.log(str);
-			}
-			pop[i].setContent(str);
+    setMapInstance(map);
+  }
 
-			L.mapquest.textMarker([gasStation[i].Coordinates.latitude, gasStation[i].Coordinates.longitude], {
-				text: gasStation[i].name,
-				subtext: 'Station Essence',
-				position: 'right',
-				type: 'marker',
-				icon: {
-				primaryColor: '#333333',
-				secondaryColor: '#333333',
-				size: 'sm'
-				}
-			}).bindPopup(pop[i]).addTo(map);
-		}
-	}
+  async function fetchNearbyStations() {
+    if (!userPosition) return;
+
+    try {
+      const response = await axios.get(`https://api.prix-carburants.2aaz.fr/stations/around/${userPosition.coords.latitude},${userPosition.coords.longitude}`);
+      setNearbyStations(response.data);
+    } catch (error) {
+      console.error("Erreur lors de la récupération des stations", error);
+    }
+  }
+
+  async function fetchStationDetails() {
+    const details = [];
+    for (let station of nearbyStations) {
+      try {
+        const response = await axios.get(`https://api.prix-carburants.2aaz.fr/station/${station.id}`);
+        details.push(response.data);
+      } catch (error) {
+        console.error('Erreur lors de la demande d\'acces aux détails de la station', error);
+        return;
+      }
+    }
+    setStationDetails(details);
+  }
+
+  function displayStationsOnMap() {
+    if (!mapInstance) return;
+    stationDetails.forEach((station) => {
+      const popupContent = generatePopupContent(station);
+      const popup = L.popup('popupPane').setContent(popupContent);
+      L.mapquest.textMarker([station.Coordinates.latitude, station.Coordinates.longitude], {
+        text: station.name,
+        subtext: 'Station Essence',
+        position: 'right',
+        type: 'marker',
+        icon: {
+          primaryColor: '#333333',
+          secondaryColor: '#333333',
+          size: 'sm'
+        }
+      }).bindPopup(popup).addTo(mapInstance);
+    });
+  }
+
+  function generatePopupContent(station: GasStation): string {
+    let content = `<h2>${station.name}</h2>\n`;
+    station.Fuels.forEach((fuel) => {
+      content += `<h3>${fuel.name}</h3>\n<p>Prix : ${fuel.Price.text} ${fuel.Price.currency}</p>\n`;
+    });
+    return content;
+  }
 
 	return (
 		<>
+
+		<div>
+			<button className='button' onClick={()=> fetchUserPosition()}>userPosition</button>
+		</div>
+		<div>
+			<button className='button' onClick={()=> initializeMap()}>initMap</button>
+		</div>
+
+			{
 			
-			<h1>Exemple de Thème Sombre</h1>
-
-			<div className="card">
-				<h1 className="headline">Headline</h1>
-				<span className="subheadline">Subheadline</span>
-				<p className="text">Text</p>
-			</div>
-
-			<button className="button"> Button</button>
-			<div>
-				<span className="error">Error</span>
-				<span className="warning">Warning</span>
-				<span className="success">Success</span>
-			</div>
-
-			<div>
+			/* <div>
 				<button className="button" onClick={() => getStation()}> Get station aroud me</button>
 			</div>
 
@@ -143,7 +147,7 @@ export default function Home() {
 			</div>
 
 			<div>
-				<button className="button" onClick={() => initMap()}> Affiche Map</button>
+				<button className="button" onClick={() => initializeMap()}> Affiche Map</button>
 			</div>
 
 			<div>
@@ -152,10 +156,10 @@ export default function Home() {
 
 			<div>
 				<button className="button" onClick={() => getGasInfo()}> Bouton test</button>
-			</div>
+			</div> */}
 
 			<div id="map">
 			</div>
 		</>
 	);
-}
+	}
